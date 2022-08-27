d7d11e91f7d9520a4ce82242a0d2f842ddf60fe7
Vivien B <vivienbihl@gmail.com>
2022-08-28 01:06:31 +0200

    Handle joint hierarchy

--- assets/convert.py
+++ assets/convert.py
@@ -43,7 +43,13 @@ ARRAY_BUFFER = 34962  # eg vertex data
 ELEMENT_ARRAY_BUFFER = 34963  # eg index data
 
 
-
+class Vertex:
+    def __init__(self):
+        self.position = []
+        self.normals = []
+        self.uvs = []
+        self.joint_ids = []
+        self.weights = []
 
 # load an example gltf file from the khronos collection
 # fname = "/home/vbihl/Documents/gltf/human.gltf"
@@ -58,6 +64,7 @@ mesh = gltf.meshes[node_id]
 # get the vertices for each primitive in the mesh (in this example there is only one)
 # TODO: handle multiple primitives (inn.gltf)
 for primitive in mesh.primitives:
+    # TODO append everything in there to a global object, to handle multiple primitives
     # get the binary data for this mesh primitive from the buffer
     accessor = gltf.accessors[primitive.attributes.POSITION]
     bufferView = gltf.bufferViews[accessor.bufferView]
@@ -75,26 +82,30 @@ for primitive in mesh.primitives:
     for i in range(accessor.count):
         index = bufferView.byteOffset + accessor.byteOffset + i*SIZE  # the location in the buffer of this vertex
         d = data[index:index+SIZE]  # the vertex data
-        v = list(struct.unpack("<fff", d))   # convert from base64 to three floats
+        v = Vertex()
+        v.position = list(struct.unpack("<fff", d))   # convert from base64 to three floats
         vertices.append(v)
 
+    # TODO: uvs, normals : make function to do the above, with certain primitive.attributes.XXX
+    # TODO: For each primite : record VertexStart,VertexCount,FaceStart,FaceCount
 
-
-    ## Joints
+    ## Joint_ids TODO : create a vertex struct to hold position, normal, uv, joint id, weights
     accessor = gltf.accessors[primitive.attributes.JOINTS_0]
-    print(accessor)
+    # print(accessor)
     bufferView = gltf.bufferViews[accessor.bufferView]
-    print(bufferView)
+    # print(bufferView)
     buffer = gltf.buffers[bufferView.buffer]
     data = gltf.get_data_from_buffer_uri(buffer.uri)
-    joints = []
+    joint_ids = []
     SIZE = COMPONENT_SIZE[accessor.componentType] * TYPE_SIZE[accessor.type]
-    print(SIZE)
+    # print(SIZE)
     for i in range(accessor.count):
         index = bufferView.byteOffset + accessor.byteOffset + i*bufferView.byteStride # TODO determine when to use SIZE vs byteStride
         d = data[index:index+SIZE]
         j = list(struct.unpack("<HHHH", d))
-        print(j)
+        # print(j)
+        joint_ids.append(j)
+        vertices[i].joint_ids = j
 
 
 
@@ -109,8 +120,9 @@ for primitive in mesh.primitives:
         index = bufferView.byteOffset + accessor.byteOffset + i*SIZE  # the location in the buffer of this vertex
         d = data[index:index+SIZE]  # the vertex data
         w = list(struct.unpack("<ffff", d))
-        print(w)
-
+        weights.append(w)
+        # print(w)
+        vertices[i].weights = w
 
 
     # ## Indices
@@ -137,7 +149,7 @@ for primitive in mesh.primitives:
 ## Skins
 ## https://github.com/KhronosGroup/glTF-Tutorials/blob/master/gltfTutorial/gltfTutorial_020_Skins.md
 skins = gltf.skins
-print(skins)
+# print(skins)
 
 for skin in skins:
     ## Inverse bind matrices
@@ -167,20 +179,54 @@ for skin in skins:
     bone 0: -1
     bone 1: 0
     """
+    jointdata = []
+    joint_hierarchy = [-1]
+    idx = 0
+    for joint in skin.joints:
+        jointdata.append({
+                'idx': idx,
+                'children': [item - 1 for item in gltf.nodes[joint].children],
+                'node_children': gltf.nodes[joint].children,
+                'rotation': gltf.nodes[joint].rotation,
+                'translation': gltf.nodes[joint].translation,
+                'scale': gltf.nodes[joint].scale,
+            })
+        idx += 1
+    # print(jointdata)
+    for joint in jointdata:
+        children = joint['children']
+        for child in children:
+            for j in jointdata:
+                if child in j['children']:
+                    joint_hierarchy.insert(child, j['idx'])
 
-
+    # print(joint_hierarchy)
 ## Animation
 ## https://github.com/KhronosGroup/glTF-Tutorials/blob/master/gltfTutorial/gltfTutorial_007_Animations.md
 
 
-print('vertices', len(vertices))
-print('triangles', len(indices))
+print('vertices:', len(vertices))
+print('triangles:', len(indices))
+print('inverse bind matrices:', len(matrices))
+print('bones:', len(joint_hierarchy))
+
+print("# Vertices")
 for vertex in vertices:
-    print(vertex[0], vertex[1], vertex[2])
+    print('position', vertex.position[0], vertex.position[1], vertex.position[2])
+    print('joint_ids', vertex.joint_ids[0], vertex.joint_ids[1], vertex.joint_ids[2], vertex.joint_ids[3])
+    print('weights', vertex.weights[0], vertex.weights[1], vertex.weights[2], vertex.weights[3])
+
+print("# Triangles")
 for index in indices:
     print(index[0], index[1], index[2])
 
+print("# Inverse bind matrices (one per bone)")
+for m in matrices:
+    print(m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8], m[9], m[10], m[11], m[12], m[13], m[14], m[15])
 
+print("# Joints hierarchy")
+for j in joint_hierarchy:
+    print(j)
 
 # convert a numpy array for some manipulation
 # S = numpy.array(vertices)
